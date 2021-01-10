8235b68c1a168bc524ee8908d3926e5997676dc4
lmz <lmz77555@gmail.com>
2021-01-11 00:00:23 +0800

    加入模板引擎

---
+++ core/src/main/java/com/cube/core/interfaces/facade/IndexController.java
@@ -0,0 +1,14 @@
+package com.cube.core.interfaces.facade;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+    @RequestMapping("")
+    public String index(){
+        return "index";
+    }
+}
